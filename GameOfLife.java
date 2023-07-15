91c1c68f41ec3363b8e93944275169794a6420ab
Asad <asad200ana@gmail.com>
2023-07-15 17:37:44 +0530

    inital template code

---
+++ GameOfLife.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class GameOfLife {
+    int size = 50;
+    boolean cellsMap[][];
+    JButton cells[][];
+
+    // Constructor
+    public GameOfLife(){
+        cellsMap = new boolean[size][size];
+        cells = new JButton[size][size];
+    }
+
+    public static void main(String[] args){
+        //Method
+        //Todo method declare
+    }
+}
